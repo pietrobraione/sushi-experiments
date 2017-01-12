cafc36a0191ef9130d0db685c60f78835a74f1bd
Pietro Braione <pietro.braione@gmail.com>
2017-01-12 17:29:17 +0100

    Add noinv treemap

---
+++ src/treemap/settings/TreemapParametersNoinv.java
@@ -0,0 +1,69 @@
+package treemap.settings;
+
+import static common.Settings.BIN_PATH;
+import static common.Settings.EVOSUITE_PATH;
+import static common.Settings.JBSE_PATH;
+import static common.Settings.OUT_PATH;
+import static common.Settings.SUSHI_LIB_PATH;
+import static common.Settings.TMP_BASE_PATH;
+import static common.Settings.Z3_PATH;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.List;
+
+import sushi.configure.Coverage;
+import sushi.configure.JBSEParameters;
+import sushi.configure.MergerParameters;
+import sushi.configure.Options;
+import sushi.configure.ParametersModifier;
+import sushi.configure.ParseException;
+import sushi.logging.Level;
+
+public class TreemapParametersNoinv extends ParametersModifier {
+	@Override
+	public void modify(Options p) {
+		//Local configurations
+		p.setEvosuitePath(EVOSUITE_PATH);
+		p.setSushiLibPath(SUSHI_LIB_PATH);
+		p.setZ3Path(Z3_PATH);
+
+		//Target 
+		p.setClassesPath(BIN_PATH, JBSE_PATH);
+		p.setTargetClass("treemap/TreeMap");
+		
+		//Analysis params 
+		p.setEvosuiteBudget(360);
+		p.setJBSEBudget(3600);
+		p.setCoverage(Coverage.BRANCHES);
+		p.setLogLevel(Level.DEBUG);
+		
+		//Tmp out directories
+		p.setOutDirectory(OUT_PATH);
+		p.setTmpDirectoryBase(TMP_BASE_PATH);
+		
+		//Parallelism
+		p.setRedundanceEvosuite(1);
+		p.setParallelismEvosuite(20);
+	}
+
+
+	@Override
+	public void modify(JBSEParameters p) 
+	throws FileNotFoundException, ParseException, IOException {
+		loadHEXFile("../sushi-experiments/settings/tree_map_noinv.jbse", p);
+		p.setHeapScope("treemap/TreeMap$Entry", 5); 				
+		p.setDepthScope(500);
+		p.setCountScope(6000);
+	}
+
+	@Override
+	public void modify(MergerParameters p) {
+		p.setBranchesToCover("treemap/TreeMap(?!.*HEXTriggers.*$).*");
+	}
+
+	@Override
+	public void modify(List<String> p) {
+		p.add("-Dobject_reuse_probability=0.8");
+	}
+}
